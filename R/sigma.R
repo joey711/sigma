0cda1a1ce9e5239d5dda25c211a063e516aaa432
Alex Crits-Christoph <acritsc1@jhu.edu>
2015-05-18 17:04:41 -0700

    latest version

--- R/sigma.R
+++ R/sigma.R
@@ -1,27 +1,14 @@
 
-# sigmajs graph widget
-#' @import rgexf
-#' @export
-edgetable_to_gexf = function(netdf){
-  # one-liner to convert edgetable to 
-  do.call(what = write.gexf, args = edge.list(netdf))
-}
-
-#' @import htmlwidgets
-#' @export
-sigma <- function(gexf, drawEdges = TRUE, drawNodes = TRUE,
-                  width = NULL, height = NULL, arrowSize='7', edgeType='curvedArrow') {
+sigma <- function(json_data, drawEdges = TRUE, drawNodes = TRUE,
+                  width = NULL, height = NULL) {
   
   # Convert the gexf object to character vector
-  data <- gexf$graph
+  data <- json_data
   
   # create a list that contains the settings
   settings <- list(
     drawEdges = drawEdges,
-    drawNodes = drawNodes,
-    minArrowSize = arrowSize,
-    defaultEdgeType = edgeType,
-    defaultLabelColor = '#006adb'
+    drawNodes = drawNodes
   )
   
 
@@ -33,15 +20,4 @@ sigma <- function(gexf, drawEdges = TRUE, drawNodes = TRUE,
   
   # create the widget
   htmlwidgets::createWidget("sigma", x, width = width, height = height)
-}
-
-#' @export
-sigmaOutput <- function(outputId, width = "100%", height = "400px") {
-  shinyWidgetOutput(outputId, "sigma", width, height, package = "sigma")
-}
-
-#' @export
-renderSigma <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  shinyRenderWidget(expr, sigmaOutput, env, quoted = TRUE)
 }
