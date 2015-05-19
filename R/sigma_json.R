c417da0f5e0ee70cacf6da374229961b55a44d61
Paul J. McMurdie <joey711@gmail.com>
2015-05-19 11:31:57 -0700

    0.2.3 fixed JSON parser?

--- R/sigma_json.R
+++ R/sigma_json.R
@@ -3,82 +3,46 @@
 #' Just need the node \code{\link{data.frame}}
 #' and the edge \code{\link{data.frame}}.
 #'
-#' @param nodeDataframe
-#' @param edgeDataframe
+#' @param nodeDataframe we need to document the required columns
+#'
+#' @param edgeDataframe same thing here.
 #'
 #' @import jsonlite
+#' @import data.table
 #' @export
-sigma_json_generator <- function(nodeDataframe, edgeDataframe){
+sigma_json_generator <- function(nodeDataframe, edgeDataframe,
+                                 palette = list(
+                                   microbe = "#3288bd",
+                                   compound = "#ff9d00",
+                                   host = "#e31a1c")
+                                 ){
+  edges$source <- as.character(edges$source)
+  edges$target <- as.character(edges$target)
   # require("jsonlite")
   edges = edgeDataframe
   nodes = nodeDataframe
-  #preset colors
-  microbeColor = "#3288bd"
-  compoundColor = "#ff9d00"
-  hostColor = "#e31a1c"
-
-  #Generate edge data for sigma
-  i = 1
-  edges_check = list()
-
-  for (so in edges$source)
-  {
-    edge = edges[i,]
-
-    backward = paste(edge$target,edge$source,sep="")
-    forward = paste(edge$source,edge$target,sep="")
-    #Have we already added an edge this pair of nodes?
-    if (backward %in% names(edges_check))
-    {
-      #It's a bi-directional edge
-      edges_check[[backward]][['type']] = 'parallel'
-      edges_check[[backward]][['label']] = paste(edge$source, edge$target, sep="<->")
-    }
-    else
-    {
-      edges_check[[forward]] = list()
-      edgeLabel = paste(edge$source, edge$target, sep="->")
-      #It's a new edge
-      edges_check[[forward]][['id']] = paste("e",toString(i),sep="")
-      edges_check[[forward]][['label']] = edgeLabel
-      edges_check[[forward]][['source']] = toString(edge$source)
-      edges_check[[forward]][['target']] = toString(edge$target)
-      edges_check[[forward]][['type']] = 'tapered'
-      edges_check[[forward]][['size']] = 1
-    }
-
-    i = i + 1
-  }
-
-  nodes_check = list()
-  #Generate node data for sigma
-  i = 1
-  for (node in nodes$id)
-  {
-    nodes_check[[node]] = list()
-    nodes_check[[node]][['id']] = node
-    if (nodes[i,]['type'] == 'microbe')
-    {
-      nodeColor = microbeColor
-    }
-    else if (nodes[i,]['type'] == 'host')
-    {
-      nodeColor = hostColor
-    }
-    else
-    {
-      nodeColor = compoundColor
-    }
-    nodes_check[[node]][['color']] = nodeColor
-    nodes_check[[node]][['x']] = nodes[i,][['x']]
-    nodes_check[[node]][['y']] = nodes[i,][['y']]
-    nodes_check[[node]][['size']] = nodes[i,][['size']]
-    i = i + 1
-  }
+  nodes$color <- unlist(palette[nodes$type])
+  # Edges
+  edges$size <- 1
+  edges$type <- "tapered"
+  edges$id <- paste0("e", 1:nrow(edges))
+  edges <- transform(edges, label = paste(source, target, sep="->"))
+  # Remove duplicated edge entries, label them as parallel
+  edges$nodeKey <- apply(edges, 1, function(x){
+    paste(sort(c(x["source"], x["target"])), collapse = "___")
+  })
+  duplicateKeysLogical = duplicated(edges$nodeKey)
+  parallelKeys = unique(edges$nodeKey[duplicateKeysLogical])
+  edges <- edges[!duplicateKeysLogical, ]
+  edges[edges$nodeKey %in% parallelKeys, ]$type <- "parallel"
+  # Use data.table to clean up this syntax
+  edgesdt = data.table(edges)
+  edgesdt[type == "parallel", label := paste(source, target, sep="<->")]
   # Make the combine JSON-formatted character to pass to sigma
   json_combined = toJSON(list(
-    nodes = unname(nodes_check),
-    edges = unname(edges_check))
-    )
+    nodes = nodes,
+    edges = edgesdt
+    ))
+  #prettify(json_combined)
   return(json_combined)
 }
