777e805b7808139c33c550569853e94f7f57d569
Paul J. McMurdie <joey711@gmail.com>
2015-05-19 13:12:36 -0700

    0.2.7 lots of fixes

--- R/sigma_json.R
+++ R/sigma_json.R
@@ -3,41 +3,54 @@
 #' Just need the node \code{\link{data.frame}}
 #' and the edge \code{\link{data.frame}}.
 #'
-#' @param nodeDataframe we need to document the required columns
+#' @param nodes we need to document the required columns
 #'
-#' @param edgeDataframe same thing here.
+#' @param edges same thing here.
+#'
+#' @param colorPalette Color Palette.
 #'
 #' @import jsonlite
 #' @import data.table
 #' @export
-sigma_json_generator <- function(nodeDataframe, edgeDataframe,
-                                 palette = list(
-                                   microbe = "#3288bd",
-                                   compound = "#ff9d00",
-                                   host = "#e31a1c")
+sigma_json_generator <- function(nodes, edges,
+                                 colorPalette = list(
+                                   Microbe = "#3288bd",
+                                   Compound = "#ff9d00",
+                                   Host = "#e31a1c")
                                  ){
-  edges$source <- as.character(edges$source)
-  edges$target <- as.character(edges$target)
-  # require("jsonlite")
-  edges = edgeDataframe
-  nodes = nodeDataframe
-  nodes$color <- unlist(palette[as.character(nodes$type)])
+  # Nodes
+  if(!any(colnames(nodes) == "label")){
+    nodes$label <- nodes$id
+  }
+  nodes$color <- unlist(colorPalette[as.character(nodes$type)])
   # Edges
+  colnames(edges)[colnames(edges) == "source"] <- "sources"
+  edges$sources <- as.character(edges$sources)
+  edges$target <- as.character(edges$target)
   edges$size <- 1
   edges$type <- "tapered"
   edges$id <- paste0("e", 1:nrow(edges))
-  edges <- transform(edges, label = paste(source, target, sep="->"))
+  edges <- transform(edges, label = paste(sources, target, sep="->"))
   # Remove duplicated edge entries, label them as parallel
   edges$nodeKey <- apply(edges, 1, function(x){
-    paste(sort(c(x["source"], x["target"])), collapse = "___")
+    paste(sort(c(x["sources"], x["target"])), collapse = "___")
   })
+  message("nodeKey: \n", paste0(edges$nodeKey, collapse = "\n"))
   duplicateKeysLogical = duplicated(edges$nodeKey)
-  parallelKeys = unique(edges$nodeKey[duplicateKeysLogical])
-  edges <- edges[!duplicateKeysLogical, ]
-  edges[edges$nodeKey %in% parallelKeys, ]$type <- "parallel"
-  # Use data.table to clean up this syntax
-  edgesdt = data.table(edges)
-  edgesdt[type == "parallel", label := paste(source, target, sep="<->")]
+message("parallel key:", edges$nodeKey[duplicateKeysLogical])
+  if(any(duplicateKeysLogical)){
+    parallelKeys = unique(edges$nodeKey[duplicateKeysLogical])
+    edges <- edges[!duplicateKeysLogical, ]
+    edges[edges$nodeKey %in% parallelKeys, ]$type <- "parallel"
+    # Use data.table to clean up this syntax
+    edgesdt = data.table(edges)
+    edgesdt[type == "parallel", label := paste(sources, target, sep="<->")]
+  } else {
+    # Use data.table to clean up this syntax
+    edgesdt = data.table(edges)
+  }
+  # Put back to source just before sending out
+  setnames(edgesdt, "sources", "source")
   # Make the combine JSON-formatted character to pass to sigma
   json_combined = toJSON(list(
     nodes = nodes,
--- inst/htmlwidgets/sigma.js
+++ inst/htmlwidgets/sigma.js
@@ -1,11 +1,11 @@
 HTMLWidgets.widget({
 
   name: "sigma",
-  
+
   type: "output",
-  
+
   initialize: function(el, width, height) {
-   
+
     // create our sigma object and bind it to the element
     sig = new sigma({
   renderer: {
@@ -16,7 +16,7 @@ HTMLWidgets.widget({
     edgeLabelSize: 'proportional',
     minEdgeSize:1,
     maxEdgeSize:3,
-    edgeLabelThreshold: 3
+    edgeLabelThreshold: 4
   }
 });
     // return it as part of our instance data
@@ -24,13 +24,13 @@ HTMLWidgets.widget({
       sig: sig
     };
   },
-  
+
   renderValue: function(el, x, instance) {
-      
+
     // parse gexf data
 /*    var parser = new DOMParser();
     var data = parser.parseFromString(x.data, "application/xml");
-*/    
+*/
     var data = x.data
     // apply settings
     for (var name in x.settings)
@@ -45,18 +45,18 @@ HTMLWidgets.widget({
     console.log(g);
     // update the sigma instance
 
-    
+
     console.log(JSON.stringify(g, null, 2));
     instance.sig.graph.read(g);
     instance.sig.refresh();
-    
+
 
   },
-  
+
   resize: function(el, width, height, instance) {
-    
+
     // forward resize on to sigma renderers
     for (var name in instance.sig.renderers)
-      instance.sig.renderers[name].resize(width, height);  
+      instance.sig.renderers[name].resize(width, height);
   }
 });
